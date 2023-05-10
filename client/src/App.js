7ad32c0b3064842a33f5ae05d2e94c47af420115
alan-augusto <augustoalan56@gmail.com>
2023-05-09 22:28:56 -0300

    Alguns streamings sem formatação

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "./App.css"
 
+
 function App() {
   useEffect(() => {
     document.documentElement.classList.add("full-height");
@@ -14,11 +15,28 @@ function App() {
       
       
       <div className= "form">
-        <label className="titles">Movie name</label>
-        <input type="text" name = "movieName"/>
-        <label className="titles">Release year</label>
-        <input type="text" name = "releaseYear"/>
-        <label>Selecione os serviços de streaming:</label>
+        <label><h2>Inserção de filme</h2></label>
+        <label className="titles">Nome do filme</label>
+        <input id="movieName"type="text" name = "movieName"/>
+        
+        <label className="titles">Ano de produção</label>
+        <input id="releaseYear" type="text" name = "releaseYear"/>
+        
+        <label className="titles">Disponóvel em</label>
+        
+        <div className="multiSelect">
+          <laberl>Netflix</laberl>
+          <input type="checkbox" id="GloboPlay"/><laberl>GloboPlay</laberl>
+          <input type="checkbox" id="PrimeVideo"/><laberl>PrimeVideo</laberl>
+          <input type="checkbox" id="HBO Max"/><laberl>HBO Max</laberl>
+          <input type="checkbox" id="Star+"/><laberl>Star+</laberl>
+          <input type="checkbox" id="Disney+"/><laberl>Disney+</laberl>
+          <input type="checkbox" id="Apple TV"/><laberl>Apple TV</laberl>
+          <input type="checkbox" id="Directv Go"/><laberl>Directv Go</laberl>
+          <input type="checkbox" id="Hulu"/><laberl>Hulu</laberl>
+        </div>
+
+        <button class="btn-13">Inseir</button>
       </div>
     </div>
   );
