c239b1789db2b06a4de127fda3916e62b5f9159a
alan-augusto <augustoalan56@gmail.com>
2023-05-09 21:01:46 -0300

    Inputs iniciais de adição de filme

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
+import "./App.css"
+
 function App() {
+  useEffect(() => {
+    document.documentElement.classList.add("full-height");
+  }, []);
+
   return (
     <div className="App">
+      <div className="header">
+        <h1>CABACEIRAS</h1>
+      </div>
+      
+      
+      <div className= "form">
+        <label className="titles">Movie name</label>
+        <input type="text" name = "movieName"/>
+        <label className="titles">Release year</label>
+        <input type="text" name = "releaseYear"/>
+        <label>Selecione os serviços de streaming:</label>
+      </div>
     </div>
   );
 }
