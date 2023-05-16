b698ab928be8b697a81a272eebc967b6083e6881
Lauraolv <oliveiraribeirolaura@icloud.com>
2023-05-16 15:47:10 -0300

    "conexão front e back - receber dados"

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
 const mysql = require("mysql");
 
@@ -8,12 +10,25 @@ const db = mysql.createPool({
     password: "Agoraufmg1",
     database: "filmes",
 });
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/", (req, res) => {
-    const sqlInsert = 
-    "INSERT INTO plataformas (nome, iconURL) VALUES ('Netflix', 'exemplo');";
-    db.query(sqlInsert, (err, result)=>{
-        res.send("hello laura");
+app.post("/", (req, res) => {
+    
+    const nome = req.body.nome
+    const ano = req.body.ano
+    const duracao = req.body.duracao
+    const genero = req.body.genero
+    const nacionalidade  = req.body.nacionalidade
+    const idioma = req.body.idioma
+    const classificacao = req.body.classificacao
+    const diretor = req.body.diretor
+    const sinopse = req.body.sinopse
+
+    const sqlInsert = "INSERT TO filmes (nome, ano, duracao, genero, nacionalidade, idioma, classificacao, diretor, sinopse, disponibilidade) VALUES (?,?)"
+    db.query(sqlInsert, [nome, ano, duracao, genero, nacionalidade, idioma, classificacao, diretor, sinopse], (err, result) =>{
+        console.log(result);
     });
 
 });
