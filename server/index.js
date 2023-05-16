40c370a5c164b99645713b4b8891be4927a4e8ee
Lauraolv <oliveiraribeirolaura@icloud.com>
2023-05-16 12:42:14 -0300

    "Teste de conexão com o banco de dados"

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "laura",
+    password: "Agoraufmg1",
+    database: "filmes",
+});
 
 app.get("/", (req, res) => {
-    res.send("hello word");
+    const sqlInsert = 
+    "INSERT INTO plataformas (nome, iconURL) VALUES ('Netflix', 'exemplo');";
+    db.query(sqlInsert, (err, result)=>{
+        res.send("hello laura");
+    });
+
 });
+
 
 app.listen(3002, () => {
     console.log("running on port 3002");
