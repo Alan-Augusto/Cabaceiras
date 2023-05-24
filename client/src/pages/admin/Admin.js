9afea4ea41c8bd1f408f43945837e0c64f8538d1
Alan Augusto <augustoalan56@gmail.com>
2023-05-23 22:37:37 -0300

    footer e alguns outros ajustes

--- client/src/pages/admin/Admin.js
+++ client/src/pages/admin/Admin.js
@@ -4,6 +4,7 @@ import './Admin.css'
 import Button from '../../common/Button/Button'
 import InputData from '../../common/InputData/InputData'
 import Header from '../../common/Header/Header'
+import Footer from '../../common/Footer/Footer'
 import axios from 'axios'
 import http from '../export'
 
@@ -138,7 +139,9 @@ return (
                 
         </div>
         <Button text="Enviar Filme" onClick={handleClick}></Button>
-        {/* <Button variant='primary' type='submit'>Tezzz</Button> */}
+        
+        <Footer/>
+
       </div>
     </div>
     
